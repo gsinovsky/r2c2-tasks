f51e71703607123f3be7c280004b155a8f3ade3b
David <david@david>
2015-12-01 10:35:00 -0430

    Remote DB configuratin

--- db.py
+++ db.py
@@ -5,10 +5,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 Base = declarative_base()
-engine = create_engine('postgresql://r2c2:r2c2@localhost/r2c2',
+#Uncomment for local connection
+#engine = create_engine('postgresql://r2c2:r2c2@localhost/r2c2',
+#                      convert_unicode=True, client_encoding='utf8')
+#Remote connection
+username = 'postgres'
+password = 'postgres'
+server = '159.90.8.39'
+engine = create_engine('postgresql://%s:%s@%s' %(username,password,server),
                        convert_unicode=True, client_encoding='utf8')
 db_session = session = scoped_session(sessionmaker(autocommit=False,
                                                    autoflush=False,
                                                    bind=engine))
-
-Base.query = db_session.query_property()
